1dc221c3ab0753cc0d2c55259817c43c4c8820ba
jwl <james.warren.leung@pm.me>
2021-12-04 14:47:38 -0800

    Day 4a complete

--- day02a/src/main.rs
+++ day02a/src/main.rs
@@ -8,27 +8,40 @@ fn calculate_final_position(input: Vec<String>) -> (i64, i64) {
         //println!("{}, {}", instructions[0], instructions[1]);
         current_pos = calculate_position(current_pos, instructions)
     }
-    return current_pos
+    return current_pos;
 }
 
 fn calculate_position(position: (i64, i64), instructions: Vec<&str>) -> (i64, i64) {
     match instructions[0] {
-        "forward" => return (position.0 + instructions[1].parse::<i64>().unwrap(), position.1),
-        "down" => return (position.0, position.1 - instructions[1].parse::<i64>().unwrap()),
-        "up" => return (position.0, position.1 + instructions[1].parse::<i64>().unwrap()),
-        _ => println!("Something fucked up.")
+        "forward" => {
+            return (
+                position.0 + instructions[1].parse::<i64>().unwrap(),
+                position.1,
+            )
+        }
+        "down" => {
+            return (
+                position.0,
+                position.1 - instructions[1].parse::<i64>().unwrap(),
+            )
+        }
+        "up" => {
+            return (
+                position.0,
+                position.1 + instructions[1].parse::<i64>().unwrap(),
+            )
+        }
+        _ => println!("Something fucked up."),
     }
     return position;
 }
-
 
 fn main() {
     let lines = aocutils::load_input_as_strings("./input.txt".to_string());
     let position = calculate_final_position(lines);
     println!("Final position is {}, {}", position.0, position.1);
-    println!("final horizontal position multiplied by inverse of y position is {}", position.0 * -position.1);
-
+    println!(
+        "final horizontal position multiplied by inverse of y position is {}",
+        position.0 * -position.1
+    );
 }
-
-
-
