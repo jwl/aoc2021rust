c1bfbb35df03f061fc471d44b0e36ed34608b9bb
jwl <james.warren.leung@pm.me>
2022-01-06 16:27:07 -0800

    Day15b complete

--- day15b/src/main.rs
+++ day15b/src/main.rs
@@ -18,14 +18,12 @@ impl MinHeap {
     fn push(&mut self, pos: (usize, usize), dist: u32) {
         self.max_heap.push((Reverse(dist), pos));
     }
-    //fn pop(&mut self) -> Option<((usize, usize), u32)> {
-    fn pop(&mut self) -> Option<(u32, (usize, usize))> {
+    fn pop(&mut self) -> Option<((usize, usize), u32)> {
         let result = self.max_heap.pop();
-        //match result {
-            //Some((Reverse(dist), pos)) => Some((pos, dist)),
-            //None => None,
-        //}
-        result.map(|(Reverse(dist), pos)| (dist, pos))
+        match result {
+            Some((Reverse(dist), pos)) => Some((pos, dist)),
+            None => None,
+        }
     }
 
     fn is_empty(&self) -> bool {
@@ -34,7 +32,7 @@ impl MinHeap {
 }
 
 impl Grid {
-    fn get_value(&self, x: usize, y: usize) -> Option<u32> {
+    fn get(&self, x: usize, y: usize) -> Option<u32> {
         let in_bounds = x < self.width && y < self.height;
         in_bounds.then(|| self.values[(x + y * self.width) as usize])
     }
@@ -42,9 +40,42 @@ impl Grid {
     fn is_valid_pos(&self, p: (usize, usize)) -> bool {
         p.0 < self.width && p.1 < self.height
     }
+
+    fn enlarge_grid(&mut self) {
+        let new_width: usize = self.width * 5;
+        let new_height: usize = self.height * 5;
+        let mut new_values: Vec<u32> = Vec::new();
+
+        for y in 0..new_height {
+            for x in 0..new_width {
+                new_values.insert(x+y * new_width, get_adjusted_risk(&self, (x, y)));
+            }
+        }
+        self.width = new_width;
+        self.height = new_height;
+        self.values = new_values;
+    }
+
+    fn print_grid(&self) {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                print!("{}", self.get(x, y).unwrap());
+            }
+            println!();
+        }
+    }
 }
 
-fn build_grid(input: &str) -> Grid {
+// returns risk of this position, given pos on the big grid, based on small grid
+fn get_adjusted_risk(grid: &Grid, pos: (usize, usize)) -> u32 {
+    let small_x: usize = pos.0 % grid.width;
+    let x_chunk: u32 = pos.0 as u32 / grid.width as u32;
+    let small_y: usize = pos.1 % grid.height;
+    let y_chunk: u32 = pos.1 as u32 / grid.height as u32;
+    (grid.get(small_x, small_y).unwrap() - 1 + x_chunk + y_chunk) % 9 + 1
+}
+
+fn build_init_grid(input: &String) -> Grid {
     let width = input.lines().next().unwrap().chars().count() as usize;
     let height = input.lines().count() as usize;
 
@@ -78,8 +109,7 @@ fn get_smallest(grid: &Grid, start: (usize, usize), end: (usize, usize)) -> u32 
     explore_queue.push(start, 0);
 
     while !explore_queue.is_empty() {
-        //let (current_pos, _) = explore_queue.pop().unwrap();
-        let (_, current_pos) = explore_queue.pop().unwrap();
+        let (current_pos, _) = explore_queue.pop().unwrap();
         let start_to_current = *distance_map.get(&current_pos).unwrap();
         //println!("current_pos is {:?}, distance from start is {:?}", current_pos, start_to_current);
 
@@ -94,7 +124,7 @@ fn get_smallest(grid: &Grid, start: (usize, usize), end: (usize, usize)) -> u32 
 
         for (i, j) in adj_pos(current_pos) {
             if grid.is_valid_pos((i, j)) {
-                let start_to_next = start_to_current + grid.get_value(i, j).unwrap();
+                let start_to_next = start_to_current + grid.get(i, j).unwrap();
                 let est_dist_from_next = start_to_next + get_manhattan((i, j), end);
                 match distance_map.get_mut(&(i, j)) {
                     // check cache to see if we've already calculated this
@@ -117,11 +147,13 @@ fn get_smallest(grid: &Grid, start: (usize, usize), end: (usize, usize)) -> u32 
 }
 
 fn main() {
-    let grid = build_grid(&aocutils::read_file_to_string(
+    let mut grid = build_init_grid(&aocutils::read_file_to_string(
         //"./sample_input.txt".to_string(),
         //"./sample_input2.txt".to_string(),
         "./input.txt".to_string(),
     ));
+
+    grid.enlarge_grid();
 
     let start = (0, 0);
     let end = (grid.width - 1, grid.height - 1);
